3551c095a7b799a976c0b7b60a88ef50f084ef50
Alexander Lamdan <alexanderlamdan2@zohomail.com>
2024-08-31 10:38:02 +0300

    Finished the clock, just need to continue with button converter from 12 hours and 24 hours & vice versa

--- projects/Clock/public/java_script/main.js
+++ projects/Clock/public/java_script/main.js
@@ -7,22 +7,40 @@ let spanMinutes = document.getElementById('minutes');
 let spanSeconds = document.getElementById('seconds');
 let format = document.getElementById('timeFormat');
 
-let clock = setInterval(function(){
+let btn24hour = document.getElementById('to24H');
+let btn12hour = document.getElementById('to12H');
+
+let clock = setInterval(()=>{
+ 
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  format.textContent = hours >= 12 ? 'pm' : 'am';
   
-  spanHours.innerText = hours;
-  spanMinutes.innerText = minutes;
-  spanSeconds.innerText = seconds;
-  
-  
-  if(spanSeconds.innerText > 59){
-    
-    spanSeconds.innerText = 0;//BUG not resets the seconds to zero
-    
-  }
-  if(spanMinutes.innerText > 59){
-    
-    spanMinutes.innerText = 0;
-    
-  }
+  spanHours.textContent = hours;
+  spanMinutes.textContent = minutes;
+  spanSeconds.textContent = seconds;
   seconds++;
+  
+  if(seconds > 59){
+    
+    seconds = 0;
+    minutes++;
+    
+  }
+  else if(minutes > 59){
+    
+    minutes = 0;
+    hours++;
+    
+  }else if(hours >= 12){
+    
+    time.toLocaleString('en-US',{hour12:true});
+    
+  }
+  
 },1000);
+
+btn24hour.addEventListener('click',function(){
+    
+  format.textContent = hours >= 12 ? '' : '';
+  
+},false);
