1bf73e4d3f288a082b4d54a1464bdd1cf87cd645
Alexander Lamdan <alexanderlamdan2@zohomail.com>
2024-09-10 21:47:51 +0300

    Calculator project is done

--- projects/Calculator/public/java_script/main.js
+++ projects/Calculator/public/java_script/main.js
@@ -1,7 +1,6 @@
 let calculator = {
   
   buttons:{
-    
     zero:document.getElementById('zero'),
     one:document.getElementById('one'),
     two:document.getElementById('two'),
@@ -20,9 +19,24 @@ let calculator = {
     equal:document.getElementById('equal'),
     multiply:document.getElementById('multiply')
   },
+  outputForm:document.getElementById('formOutput'),
   
   main(){
     
+    const clearBtn = this.buttons.clear;
+    
+    let outputForm = this.outputForm;
+    
+    clearBtn.addEventListener('click', function(){
+      
+      if(outputForm.value != ''){
+        
+        outputForm.value = '';
+        location.reload();
+        
+      }
+      
+    });
     this.numbers();
     this.mathOperations();
     //this.results();
@@ -32,13 +46,14 @@ let calculator = {
     
     const buttons = [this.buttons.zero,this.buttons.one,this.buttons.two,this.buttons.three,this.buttons.four,this.buttons.five, this.buttons.six,this.buttons.seven,this.buttons.eight,this.buttons.nine];
     
-    let output = document.getElementById('formOutput');
+    let output = this.outputForm;
     
     buttons.forEach((button)=>{
       
       button.addEventListener('click',function(){
         
         output.value += button.value;
+        return output.value;
         
       });
       
@@ -48,13 +63,49 @@ let calculator = {
   
   mathOperations:function(){
     
-    const mathSymbols = [this.buttons.clear,this.buttons.divide,this.buttons.minus,this.buttons.multiply,this.buttons.plus,this.buttons.procent];
+    const mathSymbols = [this.buttons.divide,this.buttons.minus,this.buttons.multiply,this.buttons.plus,this.buttons.procent];
+    const equalBtn = this.buttons.equal;
+    
+    let output = this.outputForm;
     
     mathSymbols.forEach(symbol=>{
       
       symbol.addEventListener('click',function(){
         
+        output.value += symbol.value;
         
+        equalBtn.addEventListener('click', function(){
+          
+          let outputStr;
+          
+          switch(symbol.value){
+          
+            case '/':
+              outputStr = String(output.value).split(symbol.value);
+              output.value = Number(outputStr[0]) / Number(outputStr[1]);
+              break;
+            case '*':
+              outputStr = String(output.value).split(symbol.value);
+              output.value = Number(outputStr[0]) * Number(outputStr[1]);
+              break;
+            case '+':
+              outputStr = String(output.value).split(symbol.value);
+              output.value = Number(outputStr[0]) + Number(outputStr[1]);
+              break;
+            case '-':
+              outputStr = String(output.value).split(symbol.value);
+              output.value = Number(outputStr[0]) - Number(outputStr[1]);
+              break;
+            case '%':
+              outputStr = String(output.value).split(symbol.value);
+              output.value = Number(outputStr[0]) % Number(outputStr[1]);
+              break;
+            default:
+              console.log('default dammit');
+            
+          }
+          
+        });
         
       });
       
