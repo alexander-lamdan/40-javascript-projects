cc8e570ea1d369f7b1c9e9d26c827177d4dc7840
Alexander Lamdan <alexanderlamdan2@zohomail.com>
2024-11-02 13:40:36 +0200

    Finished the color flipper project

--- projects/Color_flipper/public/java_script/main.js
+++ projects/Color_flipper/public/java_script/main.js
@@ -8,6 +8,7 @@
     let firstNumbers = String((Math.floor((Math.random() * 256) + 1)));
     let secondNumbers = String((Math.floor((Math.random() * 256) + 1)));
     let thirdNumbers = String((Math.floor((Math.random() * 256) + 1)));
+    
     let RGB = `RGB(${firstNumbers},${secondNumbers},${thirdNumbers})`;
     document.body.style.backgroundColor = RGB;
     let currentColorText = document.getElementById('rgb-color');
@@ -20,9 +21,24 @@
   
   btnHex.addEventListener('click',function(){
     
+    //#f1f1f1 - that what I need
     // abcdef1234567890
-    let letters = ['a','b','c','d','e','f'];
-    let numbers = String(Math.floor((Math.random() * 10) + 1));
+    
+    let letters = '0123456789ABCDEF';
+    let hexColor = '#';
+    for(let i = 0; i < 6; i++){
+      
+      hexColor += letters[(Math.floor(Math.random() * 16))];
+      
+    }
+    
+    let currentHexColor = document.getElementById('hex-color');
+    let textColor = document.querySelector('.current-color');
+    
+    document.body.style.backgroundColor = hexColor;
+    
+    textColor.style.color = 'white';
+    currentHexColor.innerText = hexColor;
     
   },false);
   
