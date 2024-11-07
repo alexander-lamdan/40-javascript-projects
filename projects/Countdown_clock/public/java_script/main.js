0190ce1d687e00a3cd177269b0d47ec72644758f
Alexander Lamdan <alexanderlamdan2@zohomail.com>
2024-11-07 19:32:29 +0200

    Now the countdown is working, but not as I need it. Will try to fix it

--- projects/Countdown_clock/public/java_script/main.js
+++ projects/Countdown_clock/public/java_script/main.js
@@ -15,13 +15,30 @@ let countdownClock = {
       
       let time = document.querySelector('.time');
       let numbers = '00:59:59';
+      let numbersArr = numbers.split(':');
       
-      numbers.split(':').forEach(function(val){
+      setInterval(()=>{
         
-        let number1 = ParseInt(val);
-        console.log(typeof number1);
+        numbersArr[2] -= 1;
+        if(numbersArr[2] < 1){
+          
+          numbersArr[2] = Number(59);
+          numbersArr[1] -= 1;
+          
+          return numbersArr[2];
+          
+        }
+        if(numbersArr[1] < 1){
+          
+          numbersArr[1] = Number(59);
+          numbersArr -= 1;
+          return numbersArr[1];
+          
+        }
         
-      });
+        time.innerText = `00:${numbersArr[1]}:${numbersArr[2]}`;
+        
+      },1000);
       
       time.innerText = numbers;
       
