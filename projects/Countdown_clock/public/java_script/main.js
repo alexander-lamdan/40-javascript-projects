f02a2ecce0e92a47b8de0d90cb795e90a8c5b68f
Alexander Lamdan <alexanderlamdan2@zohomail.com>
2024-11-09 11:18:21 +0200

    Finished the countdown timer on JavaScript, working good. As I need. Not less & not more

--- projects/Countdown_clock/public/java_script/main.js
+++ projects/Countdown_clock/public/java_script/main.js
@@ -1,51 +1,50 @@
 "use strict";
-let countdownClock = {
   
-  buttons:{
-    
-    btnStart:document.querySelector('.start-btn'),
-    btnStop:document.querySelector('.stop-btn'),
-    resetBtn:document.querySelector('.reset-btn')
-    
-  },
+const btnStart = document.querySelector('.start-btn');
+const btnReset = document.querySelector('.reset-btn');
+const stopBtn = document.querySelector('.stop-btn');
+
+let time = document.querySelector('.time');
+let numbers = '00:59:59';
+let numbersArr = numbers.split(':');
+let setTimer;
+
+btnStart.addEventListener('click',function(){
   
-  startTime:function(){
-    
-    this.buttons.btnStart.addEventListener('click',function(event){
-      
-      let time = document.querySelector('.time');
-      let numbers = '00:59:59';
-      let numbersArr = numbers.split(':');
-      
-      setInterval(()=>{
-        
-        numbersArr[2] -= 1;
-        if(numbersArr[2] < 1){
-          
-          numbersArr[2] = Number(59);
-          numbersArr[1] -= 1;
-          
-          return numbersArr[2];
-          
-        }
-        if(numbersArr[1] < 1){
-          
-          numbersArr[1] = Number(59);
-          numbersArr -= 1;
-          return numbersArr[1];
-          
-        }
-        
-        time.innerText = `00:${numbersArr[1]}:${numbersArr[2]}`;
-        
-      },1000);
-      
-      time.innerText = numbers;
-      
-      
-    },false);
-    
-  }
+  setTimer = setInterval(function(){
   
-}
-countdownClock.startTime();
+    numbersArr[2] -= 1;
+    
+    if(numbersArr[2] < 0){
+      
+      numbersArr[2] = Number(60);
+      numbersArr[1] -= 1;
+      return numbersArr[2];
+    }
+    if(numbersArr[1] < 0){
+      
+      numbersArr[1] = Number(60);
+      numbersArr[0] -= 1;
+      return numbersArr[1];
+      
+    }
+    time.innerText = numbers;
+    time.innerText = `00:${numbersArr[1]}:${numbersArr[2]}`;
+    
+  },1000);
+  
+},false);
+
+btnReset.addEventListener('click',function(){
+  
+  clearInterval(setTimer);
+  time.innerText = '00:00:00';
+  window.location.reload();
+  
+},false);
+
+stopBtn.addEventListener('click',function(){
+  
+  clearInterval(setTimer);
+  
+},false);
