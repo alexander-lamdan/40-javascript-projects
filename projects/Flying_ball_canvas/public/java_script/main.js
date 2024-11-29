7c9503ac17e6c3c704059a6fc20a5e02e089691f
Alexander Lamdan <alexanderlamdan2@zohomail.com>
2024-11-29 20:56:44 +0200

    Almost done working on Flying Ball Canvas project

--- projects/Flying_ball_canvas/public/java_script/main.js
+++ projects/Flying_ball_canvas/public/java_script/main.js
@@ -1,25 +1,38 @@
 const canvas = document.getElementById('ball').getContext('2d');
 
-(function(){
-
-  try{
-
-		canvas;
-
-	}catch(error){
-
-		console.log(error + ' Canvas is not supported');
-
-	}
-
-	function draw4Circles(){
-
-		canvas.beginPath();
-		canvas.lineWidth = 4;
-		canvas.arc(60,60,40,0,Math.PI * 2, false);
-		canvas.stroke();
-		canvas.closePath();
-
-	}
-  draw4Circles();
-})();
+function FlyingBall(x,y,radius,start,end,counterwiseClock){
+	
+	this.x = x;
+	this.y = y;
+	this.radius = radius;
+	this.start = start;
+	this.end = end;
+	this.counterwiseClock = counterwiseClock;
+	
+}
+FlyingBall.prototype.animateBall = function(){
+	
+	window.requestAnimationFrame(this.draw);
+	
+	setInterval(()=>{
+		
+		canvas.clearRect(0,0,canvas.height,canvas.width);
+		this.draw();
+		this.x += Math.floor(Math.random() * 5);
+		this.y += Math.floor(Math.random() * 5);
+		
+	},100);
+	
+}
+FlyingBall.prototype.draw = function(){
+	
+	canvas.beginPath();
+	canvas.lineWidth = 3;
+	canvas.arc(this.x,this.y,this.radius,this.start,this.end,this.counterwiseClock);
+	canvas.stroke();
+	canvas.closePath();
+	
+}
+let ball = new FlyingBall(50,50,40,0,Math.PI * 2,false);
+ball.draw();
+ball.animateBall();
