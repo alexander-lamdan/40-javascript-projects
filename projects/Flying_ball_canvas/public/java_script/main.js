429aae1ab78d753705c50d759a5bc910d16026a5
Alexander Lamdan <alexanderlamdan2@zohomail.com>
2024-11-30 12:43:06 +0200

    Getting closer to finish this project

--- projects/Flying_ball_canvas/public/java_script/main.js
+++ projects/Flying_ball_canvas/public/java_script/main.js
@@ -1,38 +1,42 @@
-const canvas = document.getElementById('ball').getContext('2d');
+const canvas = document.getElementById("ball").getContext("2d");
 
-function FlyingBall(x,y,radius,start,end,counterwiseClock){
-	
-	this.x = x;
-	this.y = y;
-	this.radius = radius;
-	this.start = start;
-	this.end = end;
-	this.counterwiseClock = counterwiseClock;
-	
+function FlyingBall(x, y, radius, start, end, counterwiseClock,wx,wy,vy,vx) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.start = start;
+  this.end = end;
+  this.counterwiseClock = counterwiseClock;
+  this.wx = wx;
+  this.wy = wy;
+  this.vy = 5;
+  this.vx = 2;
+
 }
-FlyingBall.prototype.animateBall = function(){
-	
-	window.requestAnimationFrame(this.draw);
-	
-	setInterval(()=>{
-		
-		canvas.clearRect(0,0,canvas.height,canvas.width);
-		this.draw();
-		this.x += Math.floor(Math.random() * 5);
-		this.y += Math.floor(Math.random() * 5);
-		
-	},100);
-	
-}
-FlyingBall.prototype.draw = function(){
-	
-	canvas.beginPath();
-	canvas.lineWidth = 3;
-	canvas.arc(this.x,this.y,this.radius,this.start,this.end,this.counterwiseClock);
-	canvas.stroke();
-	canvas.closePath();
-	
-}
-let ball = new FlyingBall(50,50,40,0,Math.PI * 2,false);
+
+FlyingBall.prototype.animateBall = function () {
+  
+  setInterval(()=>{
+
+    canvas.clearRect(0,0,800,500);
+    this.draw();
+    this.y += this.vy;
+    this.x += this.vx;
+    window.requestAnimationFrame(this.draw);
+    
+  },10);
+  
+};
+
+FlyingBall.prototype.draw = function () {
+  canvas.beginPath();
+  canvas.lineWidth = 3;
+  canvas.arc(this.x,this.y,this.radius,this.start,this.end,this.counterwiseClock);
+  canvas.closePath();
+  canvas.fill();
+  
+};
+
+let ball = new FlyingBall(50, 50, 40, 0, Math.PI * 2, true);
 ball.draw();
 ball.animateBall();
